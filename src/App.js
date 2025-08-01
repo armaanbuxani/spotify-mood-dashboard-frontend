07c4e26dae11f13bcc94d45f7138b495c95fc070
Armaan Buxani <armaanbuxani@gmail.com>
2025-08-01 13:47:02 -0400

    Increased stagger delay

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ function App() {
     }
   };
 
-  const staggeredLyricsRequests = async (tracks, delayMs = 3000) => {
+  const staggeredLyricsRequests = async (tracks, delayMs = 4000) => {
     const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
     const promises = tracks.map((track, index) => {
@@ -96,7 +96,7 @@ function App() {
     setShowMoodModal(true);
 
     try {
-      const lyricsResults = await staggeredLyricsRequests(tracks, 3000);
+      const lyricsResults = await staggeredLyricsRequests(tracks, 4000);
 
       const moodResults = await Promise.all(
         lyricsResults.map(async ({ track, lyrics, error }) => {
